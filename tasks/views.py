fed2b28c894ee6e93a762007d4f8d8fd093c7981
Jash Kahar <jashkahar1807@gmail.com>
2024-12-20 23:37:30 -0700

    Add tasks app with basic structure and templates

---
+++ tasks/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+tasks = [ 'Task 1', 'Task 2', 'Task 3' ]
+# Create your views here.
+def index(request):
+    return render(request, 'tasks/index.html', {
+        'tasks': tasks
+    })
